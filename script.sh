3b160626eb8afe98146b7c528a599557f13f6d16
Timani Tunduwani <mtunduwani@pivotal.io>
2017-01-31 12:13:39 -0800

    add content to file.txt

--- script.sh
+++ script.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -ev
 
-CHANGED_FILES = $(git diff --name-only HEAD...master)
-echo $(CHANGED_FILES)
-exit
-for filename in $(CHANGED_FILES); do
+CHANGED_FILES=$(git diff --name-only master...bar)
+echo "$var"
+
+for filename in "$CHANGED_FILES"; do
 	pytest scripts/frontmatter-test.py -v    
 done
 
+exit
+
+
 if [ "${TRAVIS_PULL_REQUEST}" = "true" ] && [ "$TRAVIS_BRANCH" == "prod" ]; then
-	for filename in $(CHANGED_FILES); do
+	for filename in $CHANGED_FILES; do
 		python scripts/deploy.py  
 	done	
 fi
--- scripts/frontmatter-test.py
+++ scripts/frontmatter-test.py
@@ -1,13 +1,16 @@
-import unittest, frontmatter, git, pytest
+import unittest, frontmatter, git, pytest, sys
+
+# assuming 1 file at a time
+file_name = str(sys.argv[1])
 
 class TestKBFrontmatter(unittest.TestCase):
 
     def test_has_title(self):
         "Parse frontmatter and only the frontmatter"
-        with open('tests/article_with_id.md') as f:
+        with open(file_name) as f:
             metadata, content = frontmatter.parse(f.read())
-
-        self.assertTrue('title' in metadata, "Article does not have an 'title' in the frontmatter")
+            print content
+        self.assertFalse('title' in metadata, "Article does not have an 'title' in the frontmatter")
 
     # @TODO 1. Enforce there is a template
     def test_has_template(self):
