8d7ee011200ff1de30aad46d8ee67fd0a13caa3d
Timani Tunduwani <mtunduwani@pivotal.io>
2017-02-02 23:42:13 -0800

    updating file paths

--- script.py
+++ script.py
@@ -1,41 +1,6 @@
 import frontmatter, pytest, sys, subprocess, git
-from scripts.KBFrontmatter import KBFrontmatter
-
-class KBValidator():
-
-    repository = git.Git('./')
-    base_branch = ''
-    target_branch = ''
-    is_valid = False
-
-
-    def __init__(self, ):
-        self.set_base()
-        self.set_tartget()
-
-    def gitDiff(self, branch1='', branch2=''):
-        if branch1:
-            self.set_base(branch1)
-        if branch2:
-            self.set_tartget(branch2)
-
-        format = '--name-only'
-        commits = []
-        d = self.repository.diff('%s..%s' % ('HEAD', 'master'), format).split("\n")
-
-        return d
-
-    def set_base(self, base='HEAD'):
-        if base:
-            self.base_branch = base
-
-        self.base_branch = self.repository.active_branch
-
-    def set_tartget(self, target='master'):
-        if target:
-            self.target_branch = target
-
-        self.target_branch = target
+from lib.KBFrontmatter import KBFrontmatter
+from lib.KBValidator import KBValidator
 
 
 if __name__ == '__main__':
@@ -48,7 +13,6 @@ if __name__ == '__main__':
 
     # Validate the KB frontmatter
     markup = KBFrontmatter()
-
     # Loop through the files
     for c in diff_files:
         print '-- ' + c
@@ -56,8 +20,14 @@ if __name__ == '__main__':
         if c.endswith(('.md', '.markdown')):
             print '---> ' + c + ' - is markdown: Processing...'
             markup.set_filename(c)
-            print markup.validate_markdown()
+            markup.validate_markdown()
         else:
             print '---> ' + c + ' - is not markdown: Skipping'
-
     
+    if markup.is_valid:
+        print "\n*************************\n"
+        print "Failed: %d errors Detected" % markup.is_valid
+        print "\n************************\n"
+        exit()
+
+    # print kb_validator.is_valid
