f095e569c6dc8656cb73530ad22c57ba2960df2d
Timani Tunduwani <timani@users.noreply.github.com>
2017-02-08 15:53:00 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,9 +5,7 @@ set -ev
 var=$(git diff --name-only HEAD...master)
 echo "$var"
 
-for filename in $var; do
-	python script.py 
-done
+python script.py
 
 # @TODO Add a local variable for non-travis testing
  if [ "${TRAVIS_PULL_REQUEST}" = "true" ] && [ "$TRAVIS_BRANCH" == "prod" ]; then
