c4f173b9fffb1ddd1145f417a0c7414308b7bc41
Punal Patel <punal.patel@gmail.com>
2017-02-09 15:46:46 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ echo "$var"
 python script.py
 
 # @TODO Add a local variable for non-travis testing
- if [ "$TRAVIS_BRANCH" == "prod" ]; then
+ if [ "${TRAVIS_BRANCH}" == "prod" ]; then
 	for filename in $var; do
 		python scripts/deploy.py  
 	done	
